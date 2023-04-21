505987767aceb0d2cab1c468dd1301c01e93ccbd
mathiaskrois <mathias.krois@gmail.com>
2023-04-21 09:21:49 +0200

    Changed ML script

--- ML.py
+++ ML.py
@@ -1,16 +1,25 @@
 import scipy.io 
 import numpy as np
+import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn import model_selection, linear_model
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
 import pickle
-from helper import rlr_validate, train_neural_net
-import torch
+from pprint import pprint
 
 #%% Data define
 mat = scipy.io.loadmat('training.mat')
@@ -19,24 +28,34 @@ data = mat["data"]
 y = np.load('training_labels.npy')
 X = data.reshape((data.shape[0], -1)) # reshape to 2D array
 
-N, M = X.shape
+# N, M = x.shape
 
-#%% Model 1
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.999) # % of data located to test
+# for i in range(0, N):
+#     min_val = np.min(x[i, :]) # = -11
+#     max_val = np.max(x[i, :]) # = -1
+    
+#     thresh = min_val + (min_val - max_val) * 0.3 * (-1)
+    
+#     for j in range(0, M):
+#         if x[i, j] < thresh:
+#             x[i, j] = -80
 
-# # Modeling
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2) # % of data located to test
+
+# #%% Modeling
 # model = DecisionTreeClassifier()
 # model.fit(X_train,y_train)
 
-# # Predictions
+# #%% Predictions
 # p = model.predict(X_test)
 # print(p)
 
-# # Accuracy of model 1
+# #%% Accuracy of model
 # score = accuracy_score(y_test,p)
 # print(score)
 
-#%% Model 2
+# #%% Modeling type 2
 # classifier = SVC()
 
 # parameters = [{'gamma': [0.01], 'C': [1, 10]}]
@@ -45,7 +64,8 @@ N, M = X.shape
 
 # grid_search.fit(X_train, y_train)
 
-# # Accuracy of model 2
+# #%% Accuracy of model 2
+
 # best_estimator = grid_search.best_estimator_
 
 # y_prediction = best_estimator.predict(X_test)
@@ -57,57 +77,55 @@ N, M = X.shape
 #pickle.dump(best_estimator, open('./model.p', 'wb'))
 
 #%% Model 3
-# # Normalize input data
-# scaler = StandardScaler()
-# X = scaler.fit_transform(X)
+# Normalize input data
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
 
-# # Split data into training and test sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# # Create and train model
-# model = MLPClassifier(hidden_layer_sizes=(100,50,60), max_iter=1000, alpha=0.2,
-#                       activation="relu", solver="adam", random_state=42)
-# model.fit(X_train, y_train)
+# Create and train model
+model = MLPClassifier(hidden_layer_sizes=(100,50,60), max_iter=1000, alpha=0.1,
+                      activation="relu", solver="adam", random_state=42)
+model.fit(X_train, y_train)
 
-# # Evaluate model
-# accuracy = model.score(X_test, y_test)
+# Evaluate model
+accuracy = model.score(X_test, y_test)
+print("Accuracy:", accuracy)
 
-# print("Accuracy:", accuracy)
 
-#%% Model 4: Linear Model
 
-# Using K-fold training method
-#  - - - - - - - - - - - - - - - - - - -
-# |   t e s t   |       t r a i n       |
-#  - - - - - - - - - - - - - - - - - - -
+# #%% Model 4
 
-K = 5
+# model_pipeline = []
 
-CV = model_selection.KFold(K, shuffle=True)
+# #model_pipeline.append(LogisticRegression(solver='liblinear'))
+# model_pipeline.append(SVC())
+# model_pipeline.append(KNeighborsClassifier())
+# model_pipeline.append(DecisionTreeClassifier())
+# model_pipeline.append(RandomForestClassifier())
+# model_pipeline.append(GaussianNB())
 
-for i, (train_index, test_index) in enumerate(CV.split(X,y)):
-    # define the train data and the test data
-    X_train = X[train_index]
-    y_train = y[train_index]
-    X_test = X[test_index]
-    y_test = y[test_index]
+# #%% Model 4 run
 
-    print('Test- and train data was defined')
+# #model_list = ['LR','SVM', 'KNN', 'Decision Tree', 'Random Forest', 'Naive Bayes'] 
 
-    # we find the optimal regularization parameter
-    lambdas = np.power(10.,range(-1,5)) # we set the range
-    opt_val_err, opt_lambda, mean_w_vs_lambda, train_err_vs_lambda, test_err_vs_lambda = rlr_validate(X_train, y_train, lambdas, 10)
-    print('Optimal value for lambda = ' + str(opt_lambda))
+# model_list = ['SVM', 'KNN', 'Decision Tree', 'Random Forest', 'Naive Bayes'] 
+# acc_list = []
+# auc_list = []
+# cm_list = []
+
+# for model in model_pipeline:
+#     model.fit(X_train, y_train)
+#     y_pred = model.predict(X_test)
+#     acc_list.append(metrics.accuracy_score(y_test, y_pred))
+#     #fpr, tpr, _thresholds = metrics.roc_curve(y_test, y_pred) 
+#     # auc_list.append(round(metrics.auc(fpr, tpr),2)) 
+#     cm_list.append(confusion_matrix(y_test, y_pred))
     
-    # TODO: find the correct way of applying the lambda weights
     
-    # initialize the model
-    linearModel = linear_model.LinearRegression().fit(X_train, y_train)
-    print('Model was created')
+#     # result_df = pd.DataFrame({'Model':model_list, 'Accuracy': acc_list, 'AUC': auc_list})
+# result_df = pd.DataFrame({'Model':model_list, 'Accuracy': acc_list})
+# print(result_df)
     
-    # prediction
-    y_pred = linearModel.predict(X_test)
-    
-    # estimate error rate
-    error_rate = sum((y_test - y_pred) ** 2) / N
-    print('Error rate for the linear model: ' + str(error_rate))
+
